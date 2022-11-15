a8a7f62cf89256b81d6f4d9a08cf787d9791334d
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-15 16:19:49 +0200

    infinity scroll

--- slider-project/src/App.js
+++ slider-project/src/App.js
@@ -7,6 +7,16 @@ import "./App.css";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > people.length - 1) {
+      setIndex(0);
+    }
+  }, [index, people]);
   return (
     <section className="section">
       <div className="title">
@@ -15,11 +25,20 @@ function App() {
         </h2>
       </div>
       <div className="section-center">
-        {people.map((person, index) => {
+        {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
-
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -28,10 +47,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
